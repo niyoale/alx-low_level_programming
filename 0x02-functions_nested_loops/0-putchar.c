d686360b908b25379edf25d81b390f50b825fdf5
Niyomushobozi Alexis <niyomushobozia@gmail.com>
2023-03-22 05:31:19 -0700

    task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * main -print _putschar
+ * is written by bwave
+ * Return: 0
+ */
+int main(void) 
+{
+	_putchar('_');
+	_putcha('p');
+	_putcha('u');
+	_putcha('t');
+	_putcha('c');
+	_putcha('h');
+	_putcha('a');
+	_putcha('r');
+	_putcha('\n');
+	return (0);
+}
